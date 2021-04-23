927c57f910da025e1deae8a6b6acf29774ee312a
leminhtienla <leminhtienla@gmail.com>
2021-04-23 15:52:10 +0700

    Update thcb.ino

--- thcb.ino
+++ thcb.ino
@@ -10,7 +10,7 @@ float pulse_ms = 0, rps = 0, rpm = 0, etcT = 0;
 volatile unsigned long isr_pre_ms = 0, isr_cur_ms = 0;
 
 int ETCV_table[2][10] = {
-  {120,110,100,90,80,70,60,50,40,30},
+  {120,110,100,90,  80,  70,  60,  50,  40,  30},
   {300,400,500,1000,1500,2000,2500,3000,3500,4000}
   };
 
@@ -44,7 +44,7 @@ void cal() {
 int bgA0 = analogRead(pinBG);
     bg = map(bgA0,0,1023,0,100);
 int etc = analogRead(pinETC);
-    etcV = map(bgA0,0,1023,0,5000);
+    etcV = map(etc,0,1023,0,5000);
 byte x = 1;
     etcT = map(etcV,ETCV_table[x-1][1],ETCV_table[x][1],ETCV_table[x-1][0],ETCV_table[x][0]);
     if ((micros()-isr_cur_ms) >= 1e6) {
@@ -52,7 +52,7 @@ byte x = 1;
     } else {
       pulse_ms = (isr_cur_ms - isr_pre_ms)*0.001;
     }
-    rps = teeth*1000/pulse_ms;
+    rps = 1000/pulse_ms/teeth;
     rpm = 60*rps;  
 }
 
@@ -61,7 +61,9 @@ void printSerial() {
     Serial.print(cur_ms/1000);
     Serial.print("\tloops:");
     Serial.print(loops_count);
-    Serial.print("\tspd_ms:");
+    Serial.print("\tbg:");
+    Serial.print(bg);
+    Serial.print("%\tspd_ms:");
     Serial.print(pulse_ms,3);
     Serial.print("\trps:");
     Serial.print(rps,3);
