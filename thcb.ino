5c02d54208350672e6bc05e9683111e7f4dc9fe1
leminhtienla <leminhtienla@gmail.com>
2021-04-27 08:52:02 +0700

    update

--- thcb.ino
+++ thcb.ino
@@ -2,6 +2,8 @@
 #define pinBG A0  //khai báo chân cảm biến bướm ga
 #define pinETC A1 //khai báo chân cảm biến nhiệt độ động cơ
 #define teeth 1   //khai báo số răng cảm biến tốc độ
+#define TBL_X 10
+#define TBL_Y 2
 
 unsigned long loops_count = 0;  //biến đếm số vòng lặp loops/s
 unsigned long next_ms = 0, cur_ms = 0; //
@@ -9,7 +11,7 @@ unsigned int bg = 0, etc_mV = 0;
 float pulse_ms = 0, rps = 0, rpm = 0, etcT = 0;
 volatile unsigned long isr_pre_ms = 0, isr_cur_ms = 0;
 
-int ETCV_table[2][10] = {
+int ETCV_table[TBL_Y][TBL_X] = {
   {120,110,100,90,  80,  70,  60,  50,  40,  30},  //nhiệt độ (độ C)
   {300,400,500,1000,1500,2000,2500,3000,3500,4000}  //điện áp (mmV)
   };
@@ -43,10 +45,19 @@ void loop() {
 void cal() {
 int bgA0 = analogRead(pinBG);
     bg = map(bgA0,0,1023,0,100);
-int etc = analogRead(pinETC);
-    etcV = map(etc,0,1023,0,5000);
-byte x = 1;
-    etcT = map(etcV,ETCV_table[1][x-1],ETCV_table[1][x],ETCV_table[0][x-1],ETCV_table[0][x]);
+int etcA1 = analogRead(pinETC);
+    etc_mV = map(etcA1,0,1023,0,5000);
+    if ((etc_mV >= ETCV_table[1][0]) && (etc_mV <= ETCV_table[1][TBL_X-1])) {
+      int x = 0;
+      for (x = 0; x < TBL_X; x++) {
+        if (ETCV_table[1][x] >= etc_mV) {
+          break;
+        } else {}
+      }
+      etcT = map(etc_mV,ETCV_table[1][x-1],ETCV_table[1][x],ETCV_table[0][x-1],ETCV_table[0][x]);
+    } else {
+      etcT = 0;
+    }  
     if ((micros()-isr_cur_ms) >= 1e6) {
       pulse_ms = 0;
     } else {
@@ -63,7 +74,9 @@ void printSerial() {
     Serial.print(loops_count);
     Serial.print("\tbg:");
     Serial.print(bg);
-    Serial.print("%\tspd_ms:");
+    Serial.print("%\tetcT:");
+    Serial.print(etcT,2);
+    Serial.print("\tspd_ms:");
     Serial.print(pulse_ms,3);
     Serial.print("\trps:");
     Serial.print(rps,3);
